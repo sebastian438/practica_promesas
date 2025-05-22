b354d4803deaf457bd025885461ec6bf3bdf01b4
sebastian438 <sebastian07051999@gmail.com>
2025-05-22 12:43:25 +0200

    ADD: añadida implementación de función api y promesa

--- js/main.js
+++ js/main.js
@@ -1,14 +1,45 @@
 const button = document.querySelector("#button");
-const lista = document.querySelector("#lista"); 
+const lista = document.querySelector("#lista");
 
 
+
+button.addEventListener("click", (ev) => {
+
+});
 
 
 const api = () => {
     const isOk = true;
     const promesa = new Promise((resolve, reject) => {
         if (isOk) {
-            resolve()
+            resolve({
+                nombre: "Pepe",
+                email: "pepe@gmail.com"
+            });
+        } else {
+            reject("Error");
         }
     });
+    return promesa;
 }
+
+const getApi = () => {
+    api()
+        .then((respuesta) => {
+            funcionPintar(respuesta);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
+
+const funcionPintar = () => {
+
+}
+
+getApi();
+
+
+
+
+
