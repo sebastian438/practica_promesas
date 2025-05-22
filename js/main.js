11830b81e46288a4f226f859b9553f454b4e8b5d
sebastian438 <sebastian07051999@gmail.com>
2025-05-22 11:05:06 +0200

    ADD: aniadido funcion api

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("#button");
+const lista = document.querySelector("#lista"); 
+
+
+
+
+const api = () => {
+    const isOk = true;
+    const promesa = new Promise((resolve, reject) => {
+        if (isOk) {
+            resolve()
+        }
+    });
+}
