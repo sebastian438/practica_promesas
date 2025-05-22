a22f4f5a20fb42a6133e123c60107b7937a6d032
sebastian438 <sebastian07051999@gmail.com>
2025-05-22 13:27:44 +0200

    ADD: implementación de la función pintar y todas las funciones enlazadas.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 const button = document.querySelector("#button");
 const lista = document.querySelector("#lista");
+const parrafo = document.querySelector("#parrafo");
+const fragment = document.createDocumentFragment();
 
-
+/**
+ * Evento que pone a la escucha el botón 'usuario'. El evento ejecuta la función getApi.
+ */
 
 button.addEventListener("click", (ev) => {
-
+    getApi();
 });
 
+/**
+ * La función api recibe un variable iniciadora 'isOk', una clase 'Promesa'. Después de 2 seg. se ejecuta el bloque de código que se encuentra en 'Promise'
+ * @returns Devuelve una promesa.
+ */
 
 const api = () => {
     const isOk = true;
@@ -25,21 +33,39 @@ const api = () => {
     return promesa;
 }
 
+/**
+ * Resuelve la promesa.
+ */
+
 const getApi = () => {
     api()
         .then((respuesta) => {
             funcionPintar(respuesta);
         })
         .catch((error) => {
-            console.log(error);
+            parrafo.textContent = error;
         })
 }
 
-const funcionPintar = () => {
+/**
+ * 
+ * @param {object} usuario 
+ */
 
+const funcionPintar = (usuario) => {
+    const li00 = document.createElement("LI");
+    const li01 = document.createElement("LI");
+
+    li00.textContent = usuario.nombre;
+    li01.textContent = usuario.email;
+
+    fragment.append(li00);
+    fragment.append(li01);
+
+    lista.append(fragment);
 }
 
-getApi();
+
 
 
 
